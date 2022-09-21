08a837c0f2a3602c71b9141e50c06dec441bbf57
Ernesto <ernestofamorim@gmail.com>
2022-09-21 11:18:36 -0300

    aula 21/09/2022

---
+++ SQL/usuario.sql
@@ -0,0 +1,10 @@
+create database WEB2;
+
+use WEB2;
+
+create table usuario (
+	id int(11) not null Auto_increment,
+    nome varchar(50) null default null,
+    senha varchar(50) null default null
+    
+);
